a66d1a9c7b0f0946aee853c67e35061777f27a31
Arun kumar <27963984+aruncserecs@users.noreply.github.com>
2021-05-02 00:07:20 +0530

    Update lcs.py

--- lcs.py
+++ lcs.py
@@ -2,20 +2,20 @@
 def lcs(s, t, n, m):
     # Create DP table
     dp = [[0 for i in range(m + 1)] for j in range(2)]
-    res = 0
+    result = 0
     index = 0
 
     for i in range(n + 1):
         for j in range(m + 1):
             if (s[i - 1] == t[j - 1]):
                 dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + 1
-                if (dp[i % 2][j] > res):
-                    res = dp[i % 2][j]
+                if (dp[i % 2][j] > result):
+                    result = dp[i % 2][j]
                     index = i
             else:
                 dp[i % 2][j] = 0
 
-    return res, index
+    return result, index
 
 
 # Input values
