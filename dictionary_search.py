ff08d96dc6cd8c2a6a884e431f09a5305dbb2604
Arun kumar <27963984+aruncserecs@users.noreply.github.com>
2021-05-02 00:05:35 +0530

    Update dictionary_search.py

--- dictionary_search.py
+++ dictionary_search.py
@@ -2,6 +2,6 @@ import requests
 word = input(" Enter the word : ")
 language = "en_US"
 api = f"https://api.dictionaryapi.dev/api/v2/entries/{language}/{word}"
-res = requests.get(api)
-var = res.json()
-print(var[0]['word']+'. '+var[0]['meanings'][0]['partOfSpeech']+'. '+ var[0]['meanings'][0]['definitions'][0]['definition'])
+response = requests.get(api)
+result = response.json()
+print(result[0]['word']+'. '+result[0]['meanings'][0]['partOfSpeech']+'. '+ result[0]['meanings'][0]['definitions'][0]['definition'])
