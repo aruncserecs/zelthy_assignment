a01fec2b210cc2e6d4e8e35e4ebc650d56401bc0
Arun kumar <27963984+aruncserecs@users.noreply.github.com>
2021-05-02 00:11:10 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,46 +1,24 @@
-'''
-   import smtplib
-# set the sender mail id
-sender = 'sendermailid'
-Subject = input("Subject?")
-Body = input("Body?")
-Recipient = input("Recipient?")
-
-
-try:
-   smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-   smtpObj.sendmail(sender=sender, receivers=Recipient, message=Body)
-   print("Email sent!")
-except smtplib.SMTPException:
-   print( "Error: unable to send email")
-
-   '''
-
-
-
-
-# Python code to illustrate Sending mail from
-# your Gmail account
 import smtplib
-sender = 'arunlahoriseo905@gmail.com'
-passw = 'password@gmail@'
-Subject = input("Subject?")
-Body = input("Body?")
-Recipient = input("Recipient?")
+sender = 'sender email id'
+password = 'sender_password'
+subject = input("Subject?")
+body = input("Body?")
+recipient = input("Recipient?")
 
 # creates SMTP session
-s = smtplib.SMTP('localhost')
+session = smtplib.SMTP('localhost')
 
 # start TLS for security
-s.starttls()
+session.starttls()
 
 # Authentication
-s.login(sender, passw)
+session.login(sender, password)
 
 
 # sending the mail
-s.sendmail(sender, Recipient, Body)
+session.sendmail(sender, recipient, body)
 
 # terminating the session
-s.quit()
+session.quit()
+print("Email sent!")
 
